a51c883db61b7de89245b393ac8d90f63418ccca
fxxwol <protsivroksolana2004@gmail.com>
2023-04-23 19:37:13 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const makeGalleryItemMarkup = ({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image lazyload" loading="lazy" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('')
+gallery.innerHTML = makeGalleryMarkup
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
