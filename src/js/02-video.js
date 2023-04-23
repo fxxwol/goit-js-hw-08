dd6069b508175ee1ea218723f96bf9ff3aff5621
fxxwol <protsivroksolana2004@gmail.com>
2023-04-23 21:10:26 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000))
+
+function onTimeUpdate(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+window.onload = player.setCurrentTime(localStorage['videoplayer-current-time']).then(function () {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
